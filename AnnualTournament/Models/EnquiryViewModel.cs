6bfe84c1889e37cdaf6341b3fb183d7500a874c4
gtd4 <gtd005@gmail.com>
2017-06-11 19:25:51 +1200

    adding flyer image

---
+++ AnnualTournament/Models/EnquiryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnnualTournament.Models
+{
+	public class EnquiryViewModel
+	{
+		public string Name { get; set; }
+		public string PhoneNumber { get; set; }
+		public string EmailAddress { get; set; }
+		public string Message { get; set; }
+	}
+}
