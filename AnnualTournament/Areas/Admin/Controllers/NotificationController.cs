4ade3ff90b6ab1485e42f4d6330db255fb2d2a6c
gtd4 <gtd005@gmail.com>
2017-06-18 20:22:43 +1200

    adding notification capability

---
+++ AnnualTournament/Areas/Admin/Controllers/NotificationController.cs
@@ -0,0 +1,62 @@
+﻿using AnnualTournament.Areas.Admin.Models;
+using AnnualTournament.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnnualTournament.Areas.Admin.Controllers
+{
+	public class NotificationController : Controller
+	{
+		// GET: Admin/Notification
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public ActionResult SendNotification(string message)
+		{
+			var fromAddress = new MailAddress(ConfigurationManager.AppSettings["AdminEmail"], ConfigurationManager.AppSettings["AdminName"]);
+
+			using (var msg = new MailMessage())
+			{
+				msg.Body = message;
+				msg.From = fromAddress;
+
+				if (ModelState.IsValid)
+				{
+					using (var db = new ExpressionOfInterestContext())
+					{
+						foreach (var eoi in db.ExpressionsOfInterest)
+						{
+							msg.To.Add(eoi.TeamEmailAddress);
+						}
+					}
+
+					var smtp = new SmtpClient
+					{
+						Host = "smtp.gmail.com",
+						Port = 587,
+						EnableSsl = true,
+						DeliveryMethod = SmtpDeliveryMethod.Network,
+						UseDefaultCredentials = false,
+						Credentials = new NetworkCredential(fromAddress.Address, "tournament")
+					};
+
+					{
+						smtp.Send(msg);
+					}
+				}
+			}
+			return RedirectToAction("Index", "ExpressionOfInterests");
+		}
+	}
+}
---
+++ AnnualTournament/Areas/Admin/Models/Notification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnnualTournament.Areas.Admin.Models
+{
+	public class Notification
+	{
+		public string Message { get; set; }
+	}
+}
