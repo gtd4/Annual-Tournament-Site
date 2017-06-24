85f39b941fd9ec16959019e056c606962a58fb59
gtd4 <gtd005@gmail.com>
2017-06-24 22:11:43 +1200

    adding admin users functionality

--- AnnualTournament/Areas/Admin/Controllers/AdminUserController.cs
+++ AnnualTournament/Areas/Admin/Controllers/AdminUserController.cs
@@ -1,4 +1,7 @@
-﻿using AnnualTournament.Models;
+﻿using AnnualTournament.Areas.Admin.Models;
+using AnnualTournament.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -13,11 +16,73 @@ namespace AnnualTournament.Areas.Admin.Controllers
 	public class AdminUserController : Controller
 	{
 		private ApplicationDbContext db = new ApplicationDbContext();
+		private ApplicationUserManager _userManager;
+		private const string tempAdminPassword = "Password1";
+
+		public AdminUserController()
+		{
+		}
+
+		public AdminUserController(ApplicationUserManager userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public ApplicationUserManager UserManager
+		{
+			get
+			{
+				return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+			}
+			private set
+			{
+				_userManager = value;
+			}
+		}
 
 		// GET: Admin/AdminUser
 		public async Task<ActionResult> Index()
 		{
 			return View(await db.Users.ToListAsync());
+		}
+
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create(AdminUserCreateViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+				var result = await UserManager.CreateAsync(user, tempAdminPassword);
+				if (result.Succeeded)
+				{
+					// For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+					// Send an email with this link
+					// string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+					// var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+					// await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+					return RedirectToAction("Index", "AdminUser");
+				}
+
+				AddErrors(result);
+			}
+
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
+
+		private void AddErrors(IdentityResult result)
+		{
+			foreach (var error in result.Errors)
+			{
+				ModelState.AddModelError("", error);
+			}
 		}
 	}
 }
---
+++ AnnualTournament/Areas/Admin/Models/AdminUserCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnnualTournament.Areas.Admin.Models
+{
+	public class AdminUserCreateViewModel
+	{
+		[Required]
+		[EmailAddress]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+	}
+}
